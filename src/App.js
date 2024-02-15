586f9d00b27fe46db90453f73909f26301cc2342
abdelrahman-khaled-nvtc <118051713+abdelrahman-khaled-nvtc@users.noreply.github.com>
2024-02-15 06:53:07 +0000

    Student Card

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import Card from './components/Card';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header gap-y-3">
+      <Card/>
+      <Card/>
+      <Card/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
