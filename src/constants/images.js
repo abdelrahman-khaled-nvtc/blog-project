70fa9d9bcfd4f613cc8b011888d8dbbc278d1d31
abdelrahman-khaled-nvtc <118051713+abdelrahman-khaled-nvtc@users.noreply.github.com>
2024-03-03 09:48:28 +0000

    7 - CTA

--- src/constants/images.js
+++ src/constants/images.js
@@ -1,10 +1,16 @@
 import Logo from '../assets/Logo.svg'
 import HeroImage from '../assets/HeroImage.svg'
-
-
+import Post1Image from "../assets/posts/post1.jpg";
+import PostProfileImage from "../assets/posts/post-profile.png";
+import Wave from "../assets/Wave.svg";
+import CtaImage from "../assets/CtaImage.png";
 const images = {
-    Logo,
-    HeroImage,
+  Logo,
+  HeroImage,
+  Post1Image,
+  PostProfileImage,
+  Wave,
+  CtaImage,
 };
 
 export default images;
